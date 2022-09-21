0d3f087150335f31c4889fde16d0302cb21ab7f3
aR <abenezerkifle000@gmail.com>
2022-09-21 11:49:42 +0300

    Complted Navbar

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -14,23 +14,47 @@ export default function Navbar() {
       {/* right navabr */}
       <div className="search">
         <BiSearch />
-        <input type="text" placeholder="Search" />
+        <input type="text" placeholder="type here to search" />
       </div>
     </Nav>
   );
 }
 const Nav = styled.nav`
-display:flex;
-justify-content: space-between;
-color: white;
-.title{
+  display: flex;
+  justify-content: space-between;
+  color: white;
+  .title {
     h1 {
-        span{
-            margin-left: 0.5rem;
-             color: #ffc107;
-             letter-spacing: 0.2rem;
-             font-family: "Permanent Marker", cursive;
-        }
+      span {
+        margin-left: 0.5rem;
+        color: #ffc107;
+        letter-spacing: 0.2rem;
+        font-family: "Permanent Marker", cursive;
+      }
     }
-}
+  }
+  .search {
+    background-color: #212121;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem 8rem 1rem 1rem;
+    border-radius: 1rem;
+    svg {
+      color: #ffc107;
+    }
+    input {
+      background-color: transparent;
+      border: none;
+      color: #ffc107;
+      letter-spacing: 0.3rem;
+      &::placeholder{
+        font-family: "Permanent Marker", cursive;
+        color: #ffc107;
+      }
+      &:focus{
+        outline: none;
+      }
+    }
+  }
 `;
