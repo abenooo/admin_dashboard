6e973900664855a4d20fb89fe998eff4c83df89d
aR <abenezerkifle000@gmail.com>
2022-09-26 16:05:09 +0300

    Fineshed layout

--- src/componets/Category/ListCategory.jsx
+++ src/componets/Category/ListCategory.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
-
-function ListCategory() {
+import React from "react";
+import styled from "styled-components";
+import { buttonStyles } from "../../componets/ReusableStyles";
+import { AiOutlinePlus } from "react-icons/ai";
+export default function ListCategory() {
   return (
-    <div>ListCategory</div>
-  )
+    <Section>
+      <div className="grid">
+        {/* button */}
+        <div className="row__one">
+          <div className="add__button">
+            <div>
+              <AiOutlinePlus />
+            </div>
+            <div className="content">
+              <h5>Add Category</h5>
+            </div>
+          </div>
+        </div>
+        {/* table */}
+        <div className="row__two"></div>
+      </div>
+    </Section>
+  );
 }
 
-export default ListCategory
+const Section = styled.section`
+  margin-left: 18vw;
+  padding: 2rem;
+  height: 100%;
+  .grid {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    gap: 1rem;
+    margin-top: 2rem;
+    .row__one {
+      display: grid;
+      grid-template-columns: repeat(7, 1fr);
+      height: 50%;
+      gap: 1rem;
+    }
+    .row__two {
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      gap: 1rem;
+      height: 50%;
+      background-color: wheat;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    margin-left: 0;
+    .grid {
+      .row__one {
+        grid-template-columns: repeat(2, 1fr);
+      }
+      .row__two {
+        grid-template-columns: repeat(1, 1fr);
+      }
+    }
+  }
+  .add__button {
+    ${buttonStyles};
+    padding: 0.3rem;
+    margin-top: 0.3rem;
+    margin-right: 0.2rem;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    gap: 0.5rem;
+    transition: 0.5s ease-in-out;
+  }
+`;
