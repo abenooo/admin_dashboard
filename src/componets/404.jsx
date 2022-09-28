2d99b3f183b17126674875de694f13327275bfba
aR <abenezerkifle000@gmail.com>
2022-09-28 17:50:36 +0300

    Working on modal

--- src/componets/404.jsx
+++ src/componets/404.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { Link } from "react-router-link";
+import { Link } from "react-router-dom";
 // import Card from "./Card";
 import { GiTwirlCenter } from "react-icons/gi";
 export default function NotFound() {
   return (
-    <section>
+    <>
       {/* card one */}
-      <div className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238] ">
+      <div className="overflow-hidden h-screen w-full flex flex-col justify-center items-center bg-[#1A2238] ">
         <h1 className="text-9xl font-extrabold text-white">404</h1>
-        <div className="bg-[#FF6A3D] px-2 text-sm rounded ">Page Not Found</div>
-        <div className="flex flex-row">
+        <div className="bg-[#FF6A3D] px-2 text-sm rounded ">Page Not Found</div> 
           <Link
             to="/"
             className="text-white text-3xl underline underline-offset-4 "
           >
-          
             {/* <GiTwirlCenter /> */}
             <span >Go to Home</span>
           </Link>
-          
-        </div>
+          <div className="App h-screen flex flex-col items-center justify-center">
+   
+    </div>
       </div>
-    </section>
+     
+    </>
   );
 }
--- src/componets/Category/ListCategory.jsx
+++ src/componets/Category/ListCategory.jsx
@@ -1,28 +1,174 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { buttonStyles } from "../../componets/ReusableStyles";
 import { AiOutlinePlus } from "react-icons/ai";
+import Navbar from "../Navbar";
 export default function ListCategory() {
+  // const categoryModal = () => {
+  const [showCategoryModal, setShowCategoryModal] = useState(false);
   return (
-    <Section>
-      <div className="grid">
-        {/* button */}
-        <div className="row__one">
-          <div className="add__button">
-            <div>
-              <AiOutlinePlus />
+    <>
+      <button
+        type="button"
+        className="text-white justify-center  bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+        onClick={() => setShowCategoryModal(true)}
+      >
+        <svg
+          class="mr-2 -ml-1 w-4 h-4"
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fab"
+          data-icon="github"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 496 512"
+        >
+          <path
+            fill="currentColor"
+            d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+          ></path>
+        </svg>
+        Sign in with Github
+      </button>
+      <Navbar /> 
+
+<div className="row__two">
+    <table className="w-9/12 ml-auto text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="py-3 px-6">
+                    Product name
+                </th>
+                <th scope="col" className="py-3 px-6">
+                    Color
+                </th>
+                <th scope="col" className="py-3 px-6">
+                    Category
+                </th>
+                <th scope="col" className="py-3 px-6">
+                    Price
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple MacBook Pro 17"
+                </th>
+                <td className="py-4 px-6">
+                    Sliver
+                </td>
+                <td className="py-4 px-6">
+                    Laptop
+                </td>
+                <td className="py-4 px-6">
+                    $2999
+                </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td className="py-4 px-6">
+                    White
+                </td>
+                <td className="py-4 px-6">
+                    Laptop PC
+                </td>
+                <td className="py-4 px-6">
+                    $1999
+                </td>
+            </tr>
+            <tr className="bg-white dark:bg-gray-800">
+                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td className="py-4 px-6">
+                    Black
+                </td>
+                <td className="py-4 px-6">
+                    Accessories
+                </td>
+                <td className="py-4 px-6">
+                    $99
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+   
+      {showCategoryModal ? (
+     
+              
+        <Section>
+
+          <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                  <h3 className="text-3xl font=semibold">General Info</h3>
+                  <button
+                    className="bg-transparent border-0 text-black float-right"
+                    onClick={() => setShowCategoryModal(false)}
+                  >
+                    <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
+                      x
+                    </span>
+                  </button>
+                </div>
+                <div class="py-6 px-6 lg:px-8">
+                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+                <form class="space-y-6" action="#">
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required/>
+                    </div>
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
+                    </div>
+                    <div class="flex justify-between">
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                                <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required/>
+                            </div>
+                            <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                        </div>
+                        <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+                    </div>
+                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+                    <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+                        Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                    </div>
+                </form>
             </div>
-            <div className="content">
-              <h5>Add Category</h5>
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => setShowCategoryModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="text-white bg-yellow-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => setShowCategoryModal(false)}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        {/* table */}
-        <div className="row__two"></div>
-      </div>
-    </Section>
+        </Section>
+
+      ) : null}
+    </>
   );
 }
+// }
 
 const Section = styled.section`
   margin-left: 18vw;
