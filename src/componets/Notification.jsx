6d07efb3026146367b37619c4abe5470e14fa2c6
aR <abenezerkifle000@gmail.com>
2022-09-29 14:08:12 +0300

    Add Notification componet

---
+++ src/componets/Notification.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaCog } from "react-icons/fa";
+import { BiExit } from "react-icons/bi";
+function Notification() {
+  return (
+    <div className='text-white flex flex-row mt-4 justify-between flex-wrap'>
+        <h3>Notification</h3>
+        <h3>Notification</h3>
+        <h3>Notification</h3>
+        <FaCog />
+        <BiExit />
+        </div>
+  )
+}
+
+export default Notification
