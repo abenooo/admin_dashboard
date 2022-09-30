21530764e9df63558f4ad9dae04ec1ba6287d49e
aR <abenezerkifle000@gmail.com>
2022-09-30 18:04:27 +0300

    Fix page 404

--- src/componets/404.jsx
+++ src/componets/404.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // import Card from "./Card";
-import { GiTwirlCenter } from "react-icons/gi";
+// import { GiTwirlCenter } from "react-icons/gi";
 export default function NotFound() {
   return (
     <>
       {/* card one */}
-      <div className="overflow-hidden h-screen w-full flex flex-col justify-center items-center bg-[#1A2238] ">
+      <div className="overflow-hidden h-screen w-full flex flex-col  justify-center items-center bg-[#1A2238] ">
         <h1 className="text-9xl font-extrabold text-white">404</h1>
         <div className="bg-[#FF6A3D] px-2 text-sm rounded ">Page Not Found</div> 
           <Link
