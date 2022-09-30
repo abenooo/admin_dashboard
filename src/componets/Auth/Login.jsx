f970271c251e4011519f3d4a2ee94b1baa54e28b
aR <abenezerkifle000@gmail.com>
2022-09-30 18:03:01 +0300

    Change image tag

--- src/componets/Auth/Login.jsx
+++ src/componets/Auth/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
       <div
         className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
       >
-        <img
+        <image
           src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
           className="w-full"
           alt="Sample image"
