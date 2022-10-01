12d25806a61386ac093fdd2c5b301987ab347cd2
aR <abenezerkifle000@gmail.com>
2022-10-01 09:47:42 +0300

    Fixed notifcation componet

--- src/componets/Notification.jsx
+++ src/componets/Notification.jsx
@@ -1,36 +1,93 @@
-import React from 'react'
-import { FaCog } from "react-icons/fa";
-import { BiExit } from "react-icons/bi";
-import { Link } from 'react-router-dom';
-function Notification() {
+import React from "react";
+import styled from "styled-components";
+import { CgProfile } from "react-icons/cg";
+import { AiFillCaretDown } from "react-icons/ai";
+import { IconContext } from "react-icons";
+import { MdNotificationsNone, MdOutlineLogout } from "react-icons/md";
+export default function Notification() {
   return (
-    <div className='text-white flex flex-row mt-4 justify-between flex-wrap'>
-        {/* <h3>Notification</h3>
-        <h3>Notification</h3> */}
-        <h3>Hi username</h3>
-        <FaCog />
-        <BiExit />
-        <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown divider <svg class="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-
-{/* <!-- Dropdown menu --> */}
-<div id="dropdownDivider" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-    <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDividerButton">
-      <li>
-        <Link href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</Link>
-      </li>
-      <li>
-        <Link href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</Link>
-      </li>
-      <li>
-        <Link href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</Link>
-      </li>
-    </ul>
-    <div class="py-1">
-      <Link href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Separated link</Link>
-    </div>
-</div>
+    <>
+      <Section>
+        <div className="grid">
+          {/* button */}
+          <div className="row__one">
+            <div className="notification">
+             
+              <div>
+                <MdNotificationsNone className="h-6 w-6 co" />
+              </div>
+              {/* <IconContext.Provider value={{ color: "white", size: "24" }}>
+                <div>
+                  <AiFillCaretDown />
+                </div>
+              </IconContext.Provider> */}
+              <div>
+                <AiFillCaretDown className="h-6 w-6" />
+              </div>
+              <div>
+                <CgProfile className="h-6 w-6" />
+              </div>
+              <div>
+                <MdOutlineLogout className="h-6 w-6" />
+              </div>
+              <div>
+                <image href="" className="h-6 w-6 b" />
+              </div>
+              <div className="content"></div>
+            </div>
+          </div>
         </div>
-  )
+      </Section>
+    </>
+  );
 }
 
-export default Notification
+const Section = styled.section`
+  margin-left: 16vw;
+  /* margin-right: 1.5vw; */
+  padding: 0.2rem;
+  .grid {
+    display: flex;
+    flex-direction: column;
+    gap: rem;
+    .row__one {
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      /* height: 50%; */
+      gap: 1rem;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    margin-left: 0;
+    .grid {
+      .row__one {
+        grid-template-columns: repeat(1, 1fr);
+        margin-top: -15px;
+      }
+    }
+  }
+  .notification {
+    background-color: #212121;
+    border: 0px;
+    height: 35px;
+    /* border-radius: 5px; */
+    color: white;
+    margin-top: -3px;
+    cursor: pointer;
+    display: inline-block;
+    font-family: "Permanent Marker", cursive;
+    /* font-family: system-ui,-apple-system,system-ui,"Segoe UI",Roboto,Ubuntu,"Helvetica Neue",sans-serif; */
+    font-size: 18px;
+    font-weight: 600;
+    outline: 0;
+    user-select: none;
+    -webkit-user-select: none;
+    padding: 0.3rem;
+    margin-right: 0.2rem;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    gap: 0.5rem;
+    transition: 0.5s ease-in-out;
+  }
+`;
