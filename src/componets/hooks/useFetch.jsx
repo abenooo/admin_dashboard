1d8bf19972b1e006108cebbbcba14b6c9d0a136a
aR <abenezerkifle000@gmail.com>
2022-10-08 11:07:20 +0300

    Fix minor error

--- src/componets/hooks/useFetch.jsx
+++ src/componets/hooks/useFetch.jsx
@@ -1,28 +1,30 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react"
+
 const useFetch = (url) => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
-
+      setLoading(true)
+      
       try {
-        const res = await fetch(url);
-        const json = await res.json();
-        // console.log(json)
-        // console.log("from the aapi")
-        setData(json);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  },[url]);
-  return { loading, error, data };
-};
+        const res = await fetch(url)
+        const json = await res.json()
 
-export default useFetch;
+        setData(json);
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+        setLoading(false)
+      }
+    }
+
+    fetchData();
+  }, [url])
+
+  return { loading, error, data }
+}
+
+export default useFetch
