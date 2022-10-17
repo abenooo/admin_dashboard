b3d4c92c3582c154be5a6e77ed6ff16b565fc320
aR <abenezerkifle000@gmail.com>
2022-10-17 11:23:06 +0300

    Fineshed FAQ

--- src/componets/Client/FAQ.jsx
+++ src/componets/Client/FAQ.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
-
+import React from "react";
+import Nav from "./Navbar";
+import Footer from "./Footer";
 function FAQ() {
   return (
-    <div>FAQ</div>
-  )
+    <div>
+      <Nav />
+      <section className="text-gray-700">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="text-center mb-20">
+            <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
+              Frequently Asked Question
+            </h1>
+            <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+              The most common questions about how our business works and what
+              can do for you.
+            </p>
+          </div>
+          <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div className="w-full lg:w-1/2 px-4 py-2">
+              <details className="mb-4">
+                <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  ዕድሜያቸው 18 ዓመት እና ከዚያ በላይ ለሆኑ ልጆች for account ለመክፈት የባንኩ አሠራር
+                  ይፈቅዳል? ካልፈቀደ ምክንያቱ ምንድነው?
+                </summary>
+
+                <span>
+                  For account ከዚህ በፊትም በልምድ የተጀመረ እንጂ በመመሪያ ተደግፎ እንደአሠራር የተጀመረ
+                  ነገር አልነበረም፡፡ አሁን ባለው መመሪያ ይህ አሠራር የተፈቀደ አይደለም፡፡ ምክንያቱም፤ For
+                  account ሒሳቡ የተከፈተለትን ግለሰብ መብት የሚያስጠብቅ አይደለም፡፡ ለምሳሌ፣ ለልጁ ወይም
+                  ለሠራተኛው እንዲህ ዓይነት ሒሳብ የከፈተ ሰው ቢሞት፣ የተከፈተለት ሰው ባለመብት ሆኖ ሊጠይቅ
+                  አይችልም፡፡ በሕጉ መሠረት 18 ዓመት እና ከዚያ በላይ ለሆኑ ልጆች በራሳቸው ሒሳብ ከፍተው
+                  የማንቀሳቀስ መብት ስላላቸው፤ ሞግዚት (“ስለ” ብሎ የሚከፍትላቸው ሰው) አያስፈልጋቸውም ዕድሜያቸው
+                  ከ18 ዓመት በታች ለሆኑት ግን “ያልደረሱ” (minor) በመሆናቸው minor ሒሳብ ወላጅ/አሳዳጊ
+                  ወይም ሌላ የሚመለከተው ሰው ሊከፍትላቸው ይችላል፡፡ በ minor ሒሳብ ውስጥ ሒሳብ ከፋቹ ፈራሚ
+                  (ሒሳቡን የሚያንቀሳቅስ) እንጂ የሒሳቡ ባለቤት አይደለም፡፡
+                </span>
+              </details>
+              <details className="mb-4">
+                <summary className="font-semibold bg-gray-200 rounded-md py-2 px-4">
+                  Can I see the details about archive category?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details className="mb-4">
+                <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  How can I get the archive?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+            </div>
+            <div className="w-full lg:w-1/2 px-4 py-2">
+             
+              <details className="mb-4">
+                <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  በአንድ ግለሰብ አካዉንት ላይ ወይም በአንድ ድርጅት አካውንት ላይ በተለያዩ ምክንያቶች
+                  እንዳይንቀሳቀስ የማሳገድ ስልጣን ከፍርድ ቤት ዉጪ የኢትዮጵያ ገቢዎችና ጉምሩክ ባለስልጣን እና
+                  ፀረ-ሙስና መስሪያ ቤት ማዘዝ ይችላሉ ወይ? የሚችሉም ከሆነ ከእነኚህ ዉጪ ሌሎች ካሉ ብትጠቅሱልን
+                  !
+                </summary>
+
+                <span className="px-4 py-2">
+                  ሒሳብ የማገድ ሥልጣን ያላቸው የሚከተሉት ናቸው፤ ፍርድቤት፣ የፌዴራል የሥነምግባርና የፀረ-ሙስና
+                  ኮሚሽን፣ ገቢዎች(ጉምሩክ)፣ ፌዴራል ዓቃቤ ሕግ፣ የበጎ አድራጎት ማኅበራት ኤጀንሲ(የዕርዳታ
+                  ድርጅቶችን ሒሳብ ብቻ)፣ እና በጊዜው በሕግ ሥልጣን የተሰጠው አካል (ለምሳሌ፣ በአስቸኳይ ጊዜ
+                  አዋጁ ኮማንድ ፖስት)
+                </span>
+              </details>
+              <details className="mb-4">
+                <summary className="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  How can I communicate with you?
+                </summary>
+
+                <span className="px-4 py-2">
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default FAQ
+export default FAQ;
